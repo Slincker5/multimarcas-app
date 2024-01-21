77be2aaeb5a44982ca634d9477b0430bd4099b0a
Gerson Borja <gerson.borja2018@gmail.com>
2024-01-21 16:14:13 -0600

    vista pago exitoso

--- src/router/index.js
+++ src/router/index.js
@@ -1,227 +1,239 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue'),
+      path: "/",
+      name: "home",
+      component: () => import("../views/HomeView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Panel de Administracion"
-      }
+        title: "Panel de Administracion",
+      },
     },
     {
-      path: '/crear-cintillos',
-      name: 'crearcintillos',
-      component: () => import('../views/CrearCintillosView.vue'),
+      path: "/crear-cintillos",
+      name: "crearcintillos",
+      component: () => import("../views/CrearCintillosView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Crear Cintillos"
-      }
+        title: "Crear Cintillos",
+      },
     },
     {
-      path: '/cintillos',
-      name: 'cintillos',
-      component: () => import('../views/ListaCintillosView.vue'),
+      path: "/pago-exitoso",
+      name: "crearcintillos",
+      component: () => import("../views/PagoExitosoView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Lista de Cintillos"
-      }
+        title: "Pago Exitoso",
+      },
     },
     {
-      path: '/crear-afiches',
-      name: 'crearafiches',
-      component: () => import('../views/CrearAfichesView.vue'),
+      path: "/cintillos",
+      name: "cintillos",
+      component: () => import("../views/ListaCintillosView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Crear Super Ofertas | Formato 1x4"
-      }
+        title: "Lista de Cintillos",
+      },
     },
     {
-      path: '/crear-afiches-mini',
-      name: 'crearafichesmini',
-      component: () => import('../views/CrearAfichesMiniView.vue'),
+      path: "/crear-afiches",
+      name: "crearafiches",
+      component: () => import("../views/CrearAfichesView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Crear Super Ofertas | Formato 1x9"
-      }
+        title: "Crear Super Ofertas | Formato 1x4",
+      },
     },
     {
-      path: '/crear-afiches-baja-mini',
-      name: 'crearafichesbajamini',
-      component: () => import('../views/CrearAfichesBajaMiniView.vue'),
+      path: "/crear-afiches-mini",
+      name: "crearafichesmini",
+      component: () => import("../views/CrearAfichesMiniView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Crear Afiche Baja de Precio | Formato 1x9"
-      }
+        title: "Crear Super Ofertas | Formato 1x9",
+      },
     },
     {
-      path: '/afiches-seleccion',
-      name: 'afiches-seleccion',
-      component: () => import('../views/AfichesSeleccionView.vue'),
+      path: "/crear-afiches-baja-mini",
+      name: "crearafichesbajamini",
+      component: () => import("../views/CrearAfichesBajaMiniView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Categoria de Afiches"
-      }
+        title: "Crear Afiche Baja de Precio | Formato 1x9",
+      },
     },
     {
-      path: '/afiches',
-      name: 'afiches',
-      component: () => import('../views/ListaAfichesView.vue'),
+      path: "/afiches-seleccion",
+      name: "afiches-seleccion",
+      component: () => import("../views/AfichesSeleccionView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Lista de Afiches | Formato 1x4"
-      }
+        title: "Categoria de Afiches",
+      },
     },
     {
-      path: '/afiches-mini',
-      name: 'afichesmini',
-      component: () => import('../views/ListaAfichesMiniView.vue'),
+      path: "/afiches",
+      name: "afiches",
+      component: () => import("../views/ListaAfichesView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Lista Super Oferta | Formato 1x9"
-      }
+        title: "Lista de Afiches | Formato 1x4",
+      },
     },
     {
-      path: '/lista-afiches-baja-mini',
-      name: 'listaafichesbajamini',
-      component: () => import('../views/ListaAfichesBajaMiniView.vue'),
+      path: "/afiches-mini",
+      name: "afichesmini",
+      component: () => import("../views/ListaAfichesMiniView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Lista Baja de Precio | Formato 1x9"
-      }
+        title: "Lista Super Oferta | Formato 1x9",
+      },
     },
     {
-      path: '/enviar-afiches',
-      name: 'enviarafiches',
-      component: () => import('../views/EnviarAfichesView.vue'),
+      path: "/lista-afiches-baja-mini",
+      name: "listaafichesbajamini",
+      component: () => import("../views/ListaAfichesBajaMiniView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Envio de Afiches | Formato 1x4"
-      }
+        title: "Lista Baja de Precio | Formato 1x9",
+      },
     },
     {
-      path: '/enviar-afiches-mini',
-      name: 'enviarafichesmini',
-      component: () => import('../views/EnviarAfichesMiniView.vue'),
+      path: "/enviar-afiches",
+      name: "enviarafiches",
+      component: () => import("../views/EnviarAfichesView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Envio de Afiches | Formato 1x9"
-      }
+        title: "Envio de Afiches | Formato 1x4",
+      },
     },
     {
-      path: '/enviar-afiches-baja-mini',
-      name: 'enviarafichesbajamini',
-      component: () => import('../views/EnviarAfichesBajaMini.vue'),
+      path: "/enviar-afiches-mini",
+      name: "enviarafichesmini",
+      component: () => import("../views/EnviarAfichesMiniView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Envio de Afiches Baja | Formato 1x9"
-      }
+        title: "Envio de Afiches | Formato 1x9",
+      },
     },
     {
-      path: '/editar/:uuid',
-      name: 'editarcintillo',
-      component: () => import('../views/EditarCintilloView.vue'),
+      path: "/enviar-afiches-baja-mini",
+      name: "enviarafichesbajamini",
+      component: () => import("../views/EnviarAfichesBajaMini.vue"),
       meta: {
         requiresAuth: true,
-        title: "Editar Cintillo"
-      }
+        title: "Envio de Afiches Baja | Formato 1x9",
+      },
     },
     {
-      path: '/publicacion/:post_uuid',
-      name: 'publicacion',
-      component: () => import('../views/PublicacionView.vue'),
+      path: "/editar/:uuid",
+      name: "editarcintillo",
+      component: () => import("../views/EditarCintilloView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Publicacion"
-      }
+        title: "Editar Cintillo",
+      },
     },
     {
-      path: '/enviar-cintillos',
-      name: 'enviarCintillos',
-      component: () => import('../views/EnviarCintillosView.vue'),
+      path: "/publicacion/:post_uuid",
+      name: "publicacion",
+      component: () => import("../views/PublicacionView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Enviar Cintillosw"
-      }
+        title: "Publicacion",
+      },
     },
     {
-      path: '/correo-personalizado',
-      name: 'correoPersonal',
-      component: () => import('../views/CorreoPersonalizadoView.vue'),
+      path: "/enviar-cintillos",
+      name: "enviarCintillos",
+      component: () => import("../views/EnviarCintillosView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Correo Personalizado"
-      }
+        title: "Enviar Cintillosw",
+      },
     },
     {
-      path: '/descargas',
-      name: 'generar',
-      component: () => import('../views/DescargaCintillosView.vue'),
+      path: "/correo-personalizado",
+      name: "correoPersonal",
+      component: () => import("../views/CorreoPersonalizadoView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Generar Cintillos"
-      }
+        title: "Correo Personalizado",
+      },
     },
     {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/LoginView.vue'),
-      meta: {
-        title: "MULTIMARCAS APP"
-      }
-    },
-    {
-      path: '/register',
-      name: 'registro',
-      component: () => import('../views/RegisterView.vue'),
-      meta: {
-        title: "Crear Cuenta"
-      }
-    },
-    {
-      path: '/buscador',
-      name: 'buscador',
-      component: () => import('../views/BuscadorView.vue'),
+      path: "/descargas",
+      name: "generar",
+      component: () => import("../views/DescargaCintillosView.vue"),
       meta: {
         requiresAuth: true,
-        title: "Buscador de Internos"
-      }
+        title: "Generar Cintillos",
+      },
     },
     {
-      path: '/politica-y-privacidad',
-      name: 'politicayprivacidad',
-      component: () => import('../views/PoliticaPrivacidadView.vue'),
+      path: "/login",
+      name: "login",
+      component: () => import("../views/LoginView.vue"),
       meta: {
-        title: "Politica y Privacidad"
-      }
+        title: "MULTIMARCAS APP",
+      },
     },
     {
-      path: '/suscribe',
-      name: 'suscribe',
-      component: () => import('../views/EscanerView.vue'),
+      path: "/register",
+      name: "registro",
+      component: () => import("../views/RegisterView.vue"),
       meta: {
-        requiresAuth: true
-      }
-    }
-  ]
-})
+        title: "Crear Cuenta",
+      },
+    },
+    {
+      path: "/buscador",
+      name: "buscador",
+      component: () => import("../views/BuscadorView.vue"),
+      meta: {
+        requiresAuth: true,
+        title: "Buscador de Internos",
+      },
+    },
+    {
+      path: "/politica-y-privacidad",
+      name: "politicayprivacidad",
+      component: () => import("../views/PoliticaPrivacidadView.vue"),
+      meta: {
+        title: "Politica y Privacidad",
+      },
+    },
+    {
+      path: "/suscribe",
+      name: "suscribe",
+      component: () => import("../views/EscanerView.vue"),
+      meta: {
+        requiresAuth: true,
+      },
+    },
+  ],
+});
 
 // proteger las rutas
 router.beforeEach((to, from, next) => {
   // Lógica para manejar la autenticación
   if (to.matched.some((record) => record.meta.requiresAuth)) {
-    if (!localStorage.getItem('token')) {
+    if (!localStorage.getItem("token")) {
       next({
-        path: '/login'
+        path: "/login",
       });
       return; // Asegúrate de retornar aquí para evitar que se ejecute el código siguiente
     }
   } else {
-    if (to.path === '/register' && localStorage.getItem('token') || to.path === '/login' && localStorage.getItem('token')) {
-      next('/');
+    if (
+      (to.path === "/register" && localStorage.getItem("token")) ||
+      (to.path === "/login" && localStorage.getItem("token"))
+    ) {
+      next("/");
       return; // Asegúrate de retornar aquí para evitar que se ejecute el código siguiente
     }
   }
@@ -235,5 +247,4 @@ router.beforeEach((to, from, next) => {
   next();
 });
 
-
-export default router
+export default router;
