25e82ae3471aa3edcc2cb74d621ac19c47a4c184
Gerson Borja <gerson.borja2018@gmail.com>
2024-01-21 23:06:10 -0600

    sistema de pagos agregado

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import axios from "axios";
 import { useRouter } from "vue-router";
 import { useGetRoutes } from "@/composables/getRoutes";
@@ -15,7 +15,7 @@ dayjs.extend(relativeTime);
 const router = useRouter();
 const {
   userNoPhoto,
-  userMvc,
+  userStat,
   userUpdateToken,
   postCreate,
   postList,
@@ -33,9 +33,9 @@ const rol = ref(localStorage.getItem("rol"));
 const publicacion = ref("");
 const posts = ref([]);
 const likes = ref([]);
-const estadistica = ref([]);
+const estadistica = ref(null);
 const enviando = ref(false);
-
+const modalPagos = ref("top-[-100%]");
 
 const updateToken = async () => {
   try {
@@ -43,15 +43,14 @@ const updateToken = async () => {
       Authorization: "Bearer " + token.value,
       "Content-Type": "application/json",
     };
-    const { data } = await axios.post(userUpdateToken, null, { headers })
-    localStorage.setItem("token", data.token)
+    const { data } = await axios.post(userUpdateToken, null, { headers });
+    localStorage.setItem("token", data.token);
   } catch (error) {
     console.log(error);
   }
 };
 
 updateToken();
-
 
 /*function calcularDiferencia() {
   const second = 1000,
@@ -192,6 +191,31 @@ const eliminarPost = async (post_uuid) => {
     console.log(error);
   }
 };
+const abrirModalPagos = () => {
+  modalPagos.value = "top-0";
+};
+const cerrarModalPagos = () => {
+  modalPagos.value = "top-[-100%]";
+};
+
+const obtenerEstadisticas = async () => {
+  try {
+    const headers = {
+      Authorization: "Bearer " + token.value,
+      "Content-Type": "application/json",
+    };
+    const { data } = await axios.get(userStat, { headers });
+    estadistica.value = data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+obtenerEstadisticas();
+onMounted(() => {
+  const script = document.createElement("script");
+  script.src = "https://pagos.wompi.sv/js/wompi.pagos.js";
+  document.head.appendChild(script);
+});
 </script>
 <template>
   <div>
@@ -199,6 +223,65 @@ const eliminarPost = async (post_uuid) => {
       <font-awesome-icon :icon="['fas', 'bell']" class="mr-2 text-sm" />
       <div class="text-sm font-light text-white" id="cuentaRegresiva"></div>
     </div>-->
+
+    <div
+      class="fixed left-0 z-50 w-full h-full p-4 transition-all bg-white"
+      :class="`${modalPagos}`"
+    >
+      <h3 class="flex items-center justify-between pb-4 font-medium text-black">
+        PAGOS
+        <button @click.prevent="cerrarModalPagos">
+          <font-awesome-icon :icon="['fas', 'xmark']" />
+        </button>
+      </h3>
+      <p class="pb-4 text-sm font-light text-gray-500">
+        Todas las transacciones son seguras y estan encriptadas.
+      </p>
+
+      <div>
+        <div
+          class="flex items-center justify-between border border-solid border-[#ddd] p-4"
+        >
+          <span class="text-sm font-light">Wompi El Salvador</span>
+          <div class="flex gap-1">
+            <img
+              src="https://cdn.shopify.com/shopifycloud/checkout-web/assets/0169695890db3db16bfe.svg"
+            />
+            <img
+              src="https://cdn.shopify.com/shopifycloud/checkout-web/assets/5e3b05b68f3d31b87e84.svg"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="border border-t-0 border-solid border-[#ddd] bg-gray-100">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="-270.8 371 102 52"
+          class="p-4 w-[150px] m-auto text-gray-500"
+        >
+          <path
+            fill="none"
+            stroke="currentColor"
+            stroke-miterlimit="10"
+            stroke-width="2"
+            d="M-182 404v16.8c0 .7-.4 1.2-1 1.2h-75.7c-.7 0-1.2-.6-1.2-1.2v-47.6c0-.7.6-1.2 1.2-1.2h75.7c.7 0 1 .6 1 1.2V395m-78-14h78m-17 18h27m-3.9-4.6 4.5 4.6-4.5 4.6"
+          />
+          <circle cx="-255.5" cy="376.5" r="1.5" fill="currentColor" />
+          <circle cx="-250.5" cy="376.5" r="1.5" fill="currentColor" />
+          <circle cx="-245.5" cy="376.5" r="1.5" fill="currentColor" />
+        </svg>
+        <p class="p-4 pt-0 text-xs font-light text-center text-gray-700">
+          Luego de hacer clic en “Pagar ahora”, serás redirigido a Wompi El
+          Salvador para completar tu compra de forma segura.
+        </p>
+        <div class="flex items-center justify-center p-4">
+          <div
+            class="wompi_button_widget"
+            data-url-pago="https://lk.wompi.sv/ixtR"
+          ></div>
+        </div>
+      </div>
+    </div>
     <audio class="hidden" id="audioPlayer">
       <source src="../../public/tap-like.mp3" type="audio/mp3" />
       Tu navegador no soporta el elemento de audio.
@@ -206,57 +289,79 @@ const eliminarPost = async (post_uuid) => {
     <div
       class="flex items-stretch w-full gap-1 p-4 overflow-x-auto bg-gray-100 whitespace-nowrap acciones border-b border-solid border-[#ddd]"
     >
-      <router-link
-        to="/buscador"
-        class="py-3 rounded-lg bg"
-      >
+      <router-link to="/buscador" class="py-3 rounded-lg bg">
         <div class="flex justify-center px-2 py-4">
           <img src="../../public/dairy-products.png" class="block w-[40px]" />
         </div>
-        <span class="p-2 pt-0 text-xs font-medium text-black">Buscar Internos</span>
+        <span class="p-2 pt-0 text-xs font-medium text-black"
+          >Buscar Internos</span
+        >
       </router-link>
 
-      <router-link
-        to="crear-cintillos"
-        class="py-3 rounded-lg bg"
-      >
+      <router-link to="crear-cintillos" class="py-3 rounded-lg bg">
         <div class="flex justify-center px-2 py-4">
           <img src="../../public/price-tag.png" class="block w-[40px]" />
         </div>
-        <span class="p-2 pt-0 text-xs font-medium text-black">Crear Cintillos</span>
+        <span class="p-2 pt-0 text-xs font-medium text-black"
+          >Crear Cintillos</span
+        >
       </router-link>
 
-      <router-link
-        to="/crear-afiches"
-        class="py-3 rounded-lg bg"
-      >
+      <router-link to="/crear-afiches" class="py-3 rounded-lg bg">
         <div class="flex justify-center px-2 py-4">
           <img src="../../public/rotulo-uno.png" class="block w-[40px]" />
         </div>
-        <span class="p-2 pt-0 text-xs font-medium text-black">Crear Afiches</span>
+        <span class="p-2 pt-0 text-xs font-medium text-black"
+          >Crear Afiches</span
+        >
       </router-link>
 
-      <router-link
-        to="/crear-afiches-mini"
-        class="py-3 rounded-lg bg"
-      >
+      <router-link to="/crear-afiches-mini" class="py-3 rounded-lg bg">
         <div class="flex justify-center px-2 py-4">
           <img src="../../public/rotulo-dos.png" class="block w-[40px]" />
         </div>
-        <span class="p-2 pt-0 text-xs font-medium text-black">Crear Afiches</span>
+        <span class="p-2 pt-0 text-xs font-medium text-black"
+          >Crear Afiches</span
+        >
       </router-link>
 
-      <router-link
-        to="/crear-afiches-baja-mini"
-        class="py-3 rounded-lg bg"
-      >
+      <router-link to="/crear-afiches-baja-mini" class="py-3 rounded-lg bg">
         <div class="flex justify-center px-2 py-4">
           <img src="../../public/precio_bajo.png" class="block w-[40px]" />
         </div>
-        <span class="p-2 pt-0 text-xs font-medium text-black">Crear Afiches</span>
+        <span class="p-2 pt-0 text-xs font-medium text-black"
+          >Crear Afiches</span
+        >
       </router-link>
     </div>
-
+    <div v-if="estadistica">
+      <div
+        class="flex items-center justify-between bg-green-500 border-l-4 border-green-800 border-solid"
+        v-if="estadistica.profile[0].suscripcion === 0"
+      >
+        <img
+          src="../../public/premium_account2.png"
+          alt="miembro premium"
+          class="w-[75px] block p-4 animate-bounce"
+        />
+        <div class="w-full p-4 pl-0">
+          <h3 class="text-sm font-medium text-white uppercase">
+            Suscripcion Premium
+          </h3>
+          <p class="text-sm font-light text-white">
+            Obten todos los beneficios de nuestra aplicacion
+          </p>
+          <div class="flex justify-end p-1">
+            <button
+              class="px-4 py-2 text-xs font-medium text-green-500 bg-white rounded-lg"
+              @click.prevent="abrirModalPagos"
+            >
+              Comprar Suscripcion
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
     <form class="p-4" @submit.prevent="crearPublicacion">
       <h3 class="pb-4 font-medium">PUBLICACIONES</h3>
       <div class="flex justify-between">
@@ -366,7 +471,11 @@ const eliminarPost = async (post_uuid) => {
           </button>
 
           <div>
-            <router-link :to="`/publicacion/${post.post_uuid}`" class="mr-2 text-sm text-gray-600 underline text-light">{{ post.num_comments }} Comentar</router-link>
+            <router-link
+              :to="`/publicacion/${post.post_uuid}`"
+              class="mr-2 text-sm text-gray-600 underline text-light"
+              >{{ post.num_comments }} Comentar</router-link
+            >
             <span class="text-sm text-gray-600 text-light"
               >{{ post.num_likes }} Me gusta</span
             >
@@ -410,6 +519,6 @@ const eliminarPost = async (post_uuid) => {
 }
 
 .bg:focus {
-  background-image: #383E42;
+  background-image: #383e42;
 }
 </style>
