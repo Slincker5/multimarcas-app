ca01e748019b6f08fac42b842aca6e83a40726e5
Gerson Borja <gerson.borja2018@gmail.com>
2024-02-05 06:50:32 -0600

    pwa

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import './registerServiceWorker'
 
 library.add(fas)
 
