8df25b67a63e9fef53e165dcb92f25264db71cbf
Gerson Borja <gerson.borja2018@gmail.com>
2024-03-07 22:28:26 -0600

    nuevos menu

--- src/views/CrearCintillosView.vue
+++ src/views/CrearCintillosView.vue
@@ -198,6 +198,7 @@ const agregarCintillos = async () => {
     } else {
       getData();
       reestablecerFormulario();
+      alert(foto.value)
       toast.success(data.message, {
         theme: "colored",
         autoClose: 1500,
