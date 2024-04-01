b7a76413dc83424c92ac4ecfd56d817de7bc4a72
Gerson Borja <gerson.borja2018@gmail.com>
2024-03-31 23:19:04 -0600

    tags

--- src/views/DocumentoCintillosDetallesView.vue
+++ src/views/DocumentoCintillosDetallesView.vue
@@ -11,13 +11,16 @@ dayjs.extend(relativeTime);
 const route = useRoute();
 const { etiquetas, getData } = useDocumentLabel();
 getData(route.params.path_uuid);
+const resend = () => {
+    alert("Esta funcion estara proximamente disponible..")
+}
 </script>
 
 <template>
   <div class="grid grid-cols-1">
     <div>
       <div
-        class="flex items-center justify-between gap-4 p-2 border-b border-dashed border-[#ddd]"
+        class="flex items-center justify-between gap-4 px-4 p-2 border-b border-dashed border-[#ddd]"
       >
         <div
           class="p-4 bg-green-100 border border-green-200 border-solid rounded-full shadow-lg shadow-black/20"
@@ -28,6 +31,7 @@ getData(route.params.path_uuid);
           {{ etiquetas.detalles[0].path_name }}
         </div>
       </div>
+      
       <div
         class="flex items-center w-full p-4 overflow-hidden overflow-x-auto gap-x-2 whitespace-nowrap eliminarBarraScroll"
       >
@@ -61,6 +65,22 @@ getData(route.params.path_uuid);
           {{ dayjs(etiquetas.detalles[0].fecha).fromNow() }}
         </div>
       </div>
+      <div class="flex items-center justify-between p-4 pt-0 gap-x-4">
+        <a
+              :href="`https://api.multimarcas.app/${etiquetas.detalles[0].path}`"
+              download
+              class="block w-full px-4 py-2 text-xs font-medium leading-6 text-center text-black uppercase transition bg-gray-100 rounded shadow focus:bg-gray-300 hover:bg-gray-300 ripple hover:shadow-lg focus:outline-none"
+            >
+              DESCARGAR
+            </a>
+            <button
+            @click="resend"
+              class="block w-full px-4 py-2 text-xs font-medium leading-6 text-center text-black uppercase transition bg-gray-100 rounded shadow focus:bg-gray-300 hover:bg-gray-300 ripple hover:shadow-lg focus:outline-none"
+            >
+              REENVIAR
+        </button>
+            
+      </div>
       <div class="p-4 pt-0">
         <div class="pb-2 text-sm">
           <b class="font-medium">Codigo de referencia: </b> #{{
@@ -70,10 +90,6 @@ getData(route.params.path_uuid);
         <div class="pb-2 text-sm">
           <b class="font-medium">Comentario:</b>
           {{ etiquetas.detalles[0].comentario }}
-        </div>
-        <div class="pb-0 text-sm">
-          <b class="font-medium">Generado:</b>
-          {{ dayjs(etiquetas.detalles[0].fecha).fromNow() }}
         </div>
       </div>
 
