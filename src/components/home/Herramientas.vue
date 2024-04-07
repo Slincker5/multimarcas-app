fa0f71486bc46b434e2982a3e517f4abc6e9b457
Gerson Borja <gerson.borja2018@gmail.com>
2024-04-07 14:33:09 -0600

    add news component

--- src/components/home/Herramientas.vue
+++ src/components/home/Herramientas.vue
@@ -1,44 +1,44 @@
 <script setup>
 defineProps(["premium"]);
-const rutaBase = "../../../public/";
+const rutaBase = "../../../public";
 const items = [
   {
-    image: `${rutaBase}price-tag.png`,
+    image: `${rutaBase}/price-tag.png`,
     name: "Crear Cintillos",
     href: "/crear-cintillos",
     premium: true,
     alt: "crear-cintillos",
   },
   {
-    image: `${rutaBase}dairy-products.png`,
+    image: `${rutaBase}/dairy-products.png`,
     name: "Buscar Internos",
     href: "/buscador",
     premium: false,
     alt: "buscar-internos",
   },
   {
-    image: `${rutaBase}descuento.png`,
+    image: `${rutaBase}/descuento.png`,
     name: "Afiches desc...",
     href: "/crear-afiches-mini-descuentos",
     premium: true,
     alt: "afiches-descuento",
   },
   {
-    image: `${rutaBase}rotulo-dos.png`,
+    image: `${rutaBase}/rotulo-dos.png`,
     name: "Afiches oferta",
     href: "/crear-afiches-mini",
     premium: true,
     alt: "afiches-oferta",
   },
   {
-    image: `${rutaBase}precio_bajo.png`,
+    image: `${rutaBase}/precio_bajo.png`,
     name: "Afiches de baja",
     href: "/crear-afiches-baja-mini",
     premium: true,
     alt: "afiches-de-baja",
   },
   {
-    image: `${rutaBase}rotulo-uno.png`,
+    image: `${rutaBase}/rotulo-uno.png`,
     name: "Afiches base",
     href: "/crear-afiches",
     premium: true,
