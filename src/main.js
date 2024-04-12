6c6d20881780712e4c23fca461a31b8ee5c45064
Gerson Borja <gerson.borja2018@gmail.com>
2024-04-12 17:37:32 -0600

    se agrego el service worker de la app

--- src/main.js
+++ src/main.js
@@ -12,7 +12,23 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
-
+if ('serviceWorker' in navigator) {
+    // Obtener la referencia al Service Worker que deseas desinstalar
+    navigator.serviceWorker.getRegistration('./sw.js').then(function(registration) {
+      if (registration) {
+        // Desinstalar el Service Worker
+        registration.unregister().then(function(success) {
+            alert("ok")
+          console.log('Service Worker desinstalado con éxito:', registration);
+        }).catch(function(error) {
+          console.error('Error al desinstalar el Service Worker:', error);
+        });
+      } else {
+        console.log('No se encontró el Service Worker para desinstalar');
+      }
+    });
+  }
+  
 
 library.add(fas)
 
