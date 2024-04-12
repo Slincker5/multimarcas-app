8f2d81638886b3701a909c0bafdc0581846f5fb5
Gerson Borja <gerson.borja2018@gmail.com>
2024-04-12 17:22:49 -0600

    se agrego el service worker de la app

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,5 @@
 var CACHE_NAME = 'multimarcas-pwa';
 
-let deferredPrompt;
-
-self.addEventListener('beforeinstallprompt', function(event) {
-  // Guardar el evento para mostrar la ventana de instalación automáticamente
-  deferredPrompt = event;
-  // Mostrar la ventana de instalación automáticamente
-  deferredPrompt.prompt();
-  // Esperar a que el usuario responda a la ventana de instalación
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('El usuario ha aceptado la instalación de la aplicación');
-    } else {
-      console.log('El usuario ha rechazado la instalación de la aplicación');
-    }
-    // Limpiar la variable para evitar que se muestre la ventana de instalación nuevamente
-    deferredPrompt = null;
-  });
-});
-
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
