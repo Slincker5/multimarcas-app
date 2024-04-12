fa4eba2dba8eb8e2583fc87af1a2e2a4c44e47bb
Gerson Borja <gerson.borja2018@gmail.com>
2024-04-12 16:00:18 -0600

    add service worker pwa

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import './registerServiceWorker'
+
 
 library.add(fas)
 
