9585deb9aa0ee316dc0fd90ad062234320974f9f
Gerson Borja <gerson.borja2018@gmail.com>
2024-04-12 22:16:22 -0600

    free y manifest

--- src/main.js
+++ src/main.js
@@ -11,23 +11,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import './registerServiceWorker'
 
-if ('serviceWorker' in navigator) {
-    // Obtener la referencia al Service Worker que deseas desinstalar
-    navigator.serviceWorker.getRegistration('./sw.js').then(function(registration) {
-      if (registration) {
-        // Desinstalar el Service Worker
-        registration.unregister().then(function(success) {
-          console.log('Service Worker desinstalado con éxito:', registration);
-        }).catch(function(error) {
-          console.error('Error al desinstalar el Service Worker:', error);
-        });
-      } else {
-        console.log('No se encontró el Service Worker para desinstalar');
-      }
-    });
-  }
-  
 
 library.add(fas)
 
