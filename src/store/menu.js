eb7d689ec307703916385d5550c050f74af2cb9a
Gerson Borja <gerson.borja2018@gmail.com>
2024-04-12 22:07:10 -0600

    free y manifest

--- src/store/menu.js
+++ src/store/menu.js
@@ -27,9 +27,7 @@ export const useNavBar = defineStore("navBar", () => {
         beamsClient
           .start()
           .then(() =>
-            beamsClient.addDeviceInterest(
-              'global', userData.value.profile[0].user_uuid
-            )
+            beamsClient.addDeviceInterest(userData.value.profile[0].user_uuid)
           )
           .then(() => console.log("Successfully registered and subscribed!"))
           .catch(console.error);
