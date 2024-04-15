80f7a0157c9495f47df728cf68f84728737c5aca
Gerson Borja <gerson.borja2018@gmail.com>
2024-04-14 19:33:55 -0600

    test

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,22 @@
 // service-worker.js
 
 // Escuchando el evento 'install', pero no realizando ninguna acción
-self.addEventListener('install', function(event) {
-    // No se realiza ninguna acción aquí
-  });
-  
-  // Escuchando el evento 'fetch' y respondiendo con la solicitud original sin almacenar en caché
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(fetch(event.request));
-  });
-  
+self.addEventListener("install", function (event) {
+  // No se realiza ninguna acción aquí
+});
+
+// Escuchando el evento 'fetch' y respondiendo con la solicitud original sin almacenar en caché
+self.addEventListener("fetch", function (event) {
+  event.respondWith(fetch(event.request));
+});
+
+// Add event listener for notification click
+self.addEventListener("notificationclick", function (event) {
+  if (event.action === "detalles") {
+    const ruta = `/cintillo/${event.data.uuid}`
+    event.notification.close();
+
+    // Open the URL when the notification is clicked
+    event.waitUntil(clients.openWindow(ruta));
+  }
+});
