0c65ff82df8eacb71ede8ae00b86433497169573
Gerson Borja <gerson.borja2018@gmail.com>
2024-04-14 20:06:26 -0600

    test

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -50,3 +50,14 @@ self.addEventListener('notificationclick', function(event) {
     clients.openWindow(event.notification.data.url)
   );
 });
+
+// Add event listener for notification click
+self.addEventListener("notificationclick", function (event) {
+  if (event.action === "detalles") {
+    const ruta = `/cintillo/${event.notification.data.uuid}`
+    event.notification.close();
+
+    // Open the URL when the notification is clicked
+    event.waitUntil(clients.openWindow(ruta));
+  }
+});
