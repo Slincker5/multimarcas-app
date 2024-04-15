8bf088306d71e6dc75236d671aba8f3925806f24
Gerson Borja <gerson.borja2018@gmail.com>
2024-04-14 19:59:41 -0600

    test

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,12 @@
 importScripts("https://js.pusher.com/beams/service-worker.js");
+
+// Add event listener for notification click
+self.addEventListener("notificationclick", function (event) {
+    if (event.action === "detalles") {
+      const ruta = `/cintillo/${event.notification.data.uuid}`
+      event.notification.close();
+  
+      // Open the URL when the notification is clicked
+      event.waitUntil(clients.openWindow(ruta));
+    }
+  });
