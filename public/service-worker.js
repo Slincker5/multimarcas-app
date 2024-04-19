6c01db4f42f4a151e347301e32d0b1a1f291ea97
Gerson Borja <gerson.borja2018@gmail.com>
2024-04-19 00:31:54 -0600

    no inter

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,19 +10,22 @@ self.addEventListener("fetch", function (event) {
   event.respondWith(fetch(event.request));
 });*/
 
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(cachedResponse => {
-        // Si hay una respuesta en cachÃ©, devolverla
         if (cachedResponse) {
           return cachedResponse;
         }
 
-        // Si no hay respuesta en cachÃ©, intentar obtenerla de la red
+        if (event.request.method !== 'GET') {
+          // Si la solicitud no es GET, simplemente pasarla sin intentar cachearla
+          return fetch(event.request);
+        }
+
         return fetch(event.request)
           .then(networkResponse => {
-            // Clonar la respuesta de la red antes de almacenarla en cachÃ©
             const clonedResponse = networkResponse.clone();
             caches.open('offline-cache')
               .then(cache => {
@@ -31,9 +34,8 @@ self.addEventListener('fetch', event => {
             return networkResponse;
           })
           .catch(error => {
-            // Si la solicitud de red falla, devolver una respuesta predeterminada o una respuesta almacenada en cachÃ©
             console.log('Fetch fallido:', error);
-            return caches.match('/offline.html'); // Respuesta predeterminada o pÃ¡gina offline
+            return caches.match('/offline.html');
           });
       })
   );
