e35c478c4c64104b492ddf9873620c38023f335a
Gerson Borja <gerson.borja2018@gmail.com>
2024-05-06 05:18:45 -0600

    Fix notifications

--- src/views/DownloaderView.vue
+++ src/views/DownloaderView.vue
@@ -6,12 +6,12 @@ import CargandoFrom from "@/components/globales/CargandoForm.vue";
 const busqueda = ref("");
 const resultados = ref([]);
 const enviando = ref(false);
-const texto = ref("")
+const texto = ref("");
 
 const buscar = async () => {
   try {
     enviando.value = true;
-    texto.value = "Realizando busqueda ..."
+    texto.value = "Realizando busqueda ...";
     const { data } = await axios.get(
       `https://api.multimarcas.app/api/youtube/search/${busqueda.value}`
     );
@@ -25,8 +25,8 @@ const buscar = async () => {
 
 const download = async (id, name) => {
   try {
-    enviando.value = true
-    texto.value = "Procesando descarga..."
+    enviando.value = true;
+    texto.value = "Procesando descarga...";
     const datos = {
       id: id,
     };
@@ -50,16 +50,19 @@ const download = async (id, name) => {
   } catch (error) {
     console.log(error);
   } finally {
-    enviando.value = false
+    enviando.value = false;
   }
 };
 
+const filtroTime = (duracion) => {
+  const time = duracion.split(":");
+  return parseInt(time.join(""));
+};
 
-
-const filtroTime  = (duracion) => {
-    const time = duracion.split(":")
-    return  parseInt(time.join(''));
-}
+const anuncioTmp = ref(localStorage.getItem("musica"));
+const cerrarVentana = () => {
+  anuncioTmp.value = localStorage.setItem("musica", "true");
+};
 </script>
 <template>
   <div class="overflow-y-scroll">
@@ -84,18 +87,48 @@ const filtroTime  = (duracion) => {
 
     <div class="p-4">
       <div v-for="song in resultados" :key="song.id">
-        <div class="border border-solid border-[#ddd] mb-4" v-if="song.duration !== null && filtroTime(song.duration) < 1100">
+        <div
+          class="border border-solid border-[#ddd] mb-4"
+          v-if="song.duration !== null && filtroTime(song.duration) < 1100"
+        >
           <div
             class="font-medium truncate block p-4 border-b border-dashed border-[#ddd]"
           >
             {{ song.title }}
           </div>
           <div class="flex items-center justify-between p-4">
-            <div><b class="font-medium">Duracion:</b> {{ song.duration === null ? 'Desconocida' : song.duration }}</div>
+            <div>
+              <b class="font-medium">Duracion:</b>
+              {{ song.duration === null ? "Desconocida" : song.duration }}
+            </div>
             <div>
               <button @click="download(song.id, song.title)">Descargar</button>
             </div>
           </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="fixed top-0 left-0 z-30 flex items-center justify-center w-full h-full bg-black/80"
+      v-if="anuncioTmp === null"
+    >
+      <div
+        class="bg-white w-[80%] py-6 px-4 max-w-screen-sm rounded-lg shadow-2xl"
+      >
+        <p class="py-4 text-sm text-center text-gray-500">
+          La función de descarga de música ha sido desarrollada por el
+          propietario de la aplicación como un recurso de entretenimiento.
+          Estará accesible durante el periodo que el propietario determine,
+          hasta que decida retirarla.
+        </p>
+        <div class="py-4 pb-0 border-t border-dashed border-[#ddd]">
+          <button
+            class="block px-6 py-2 mx-auto mb-4 text-sm text-center border border-solid rounded-sm shadow-lg border-neutral-700"
+            @click.prevent="cerrarVentana"
+          >
+            Cerrar ventana
+          </button>
         </div>
       </div>
     </div>
