56fb97c25707e71776a54292d5b287042d77154e
Gerson Borja <gerson.borja2018@gmail.com>
2024-05-06 05:06:01 -0600

    Fix notifications

--- src/components/menu/MenuPrincipal.vue
+++ src/components/menu/MenuPrincipal.vue
@@ -14,21 +14,21 @@ const emit = defineEmits(["openMenu"])
         >MULTIMARCAS APP</router-link
       >
       <nav class="flex gap-2">
-        <button class="relative hidden text-white">
+        <router-link to="/buscar-musica" class="relative text-white">
           <div
-            class="absolute right-[-10px] top-0 inline-flex items-center justify-center bg-rose-500 text-white text-xs font-extralight rounded-full no-underline w-[14px] h-[14px]"
-          >
-            1
+            class="absolute top-[-3px] right-[-6px]">
+            <span class="relative flex w-2 h-2">
+  <span class="absolute inline-flex w-full h-full bg-red-400 rounded-full opacity-75 animate-ping"></span>
+  <span class="relative inline-flex w-2 h-2 bg-red-500 rounded-full"></span>
+</span>
           </div>
-          <font-awesome-icon :icon="['fas', 'bell']" />
-        </button>
+          <font-awesome-icon :icon="['fas', 'music']" />
+        </router-link>
         <button
           class="relative pl-4 pr-2 md:hidden"
           active-class="underline"
           @click="$emit('openMenu')"
-        ><span
-          class="absolute top-0 right-0 inline-flex w-2 h-2 font-black rounded-full opacity-75 animate-ping bg-rose-700"
-        ></span>
+        >
           <font-awesome-icon :icon="['fas', 'bars']" />
         </button>
       </nav>
